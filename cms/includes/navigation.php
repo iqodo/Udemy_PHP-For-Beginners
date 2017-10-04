1b561cc522a048edff54fbfb5004800171b09eb8
Marco <marco@studio148.nl>
2017-10-04 16:34:59 +0200

    url aanpassen navigation.php

--- cms/includes/navigation.php
+++ cms/includes/navigation.php
@@ -1,6 +1,5 @@
    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -9,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/cms">CMS Front</a>
+                <a class="navbar-brand" href="index.php">CMS Front</a>
             </div>
             
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -25,30 +24,30 @@
                            $cat_title = $row['cat_title'];
                            $cat_id = $row['cat_id'];
                             
-                            echo "<li><a href='/cms/category/{$cat_id}'>{$cat_title}</a></li>";
+                            echo "<li><a href='category/{$cat_id}'>{$cat_title}</a></li>";
                         }
                                     
                     ?>
 
                     <?php if( isLoggedIn() ) : ?>
 
-                        <li> <a href="/cms/admin">Admin</a> </li>
-                        <li> <a href="/cms/includes/logout.php">Logout</a> </li>
+                        <li> <a href="admin">Admin</a> </li>
+                        <li> <a href="includes/logout.php">Logout</a> </li>
 
-                    <?php else: ?>
+                        <?php else: ?>
 
-                        <li> <a href="/cms/login.php">Login</a> </li>
+                            <li> <a href="login.php">Login</a> </li>
 
                     <?php endif; ?>
 
-                        <li> <a href="/cms/registration">Registration</a> </li>
+                        <li> <a href="registration">Registration</a> </li>
                         
                     <?php 
 
                         if(isset($_SESSION['user_role'])) {                    
                             if(isset($_GET['p_id'])) {
                                 $the_post_id = $_GET['p_id'];
-                                echo "<li><a href='/cms/admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
+                                echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
                             }
                         }
                     
